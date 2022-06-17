159d4220e61ee50c0bdac7b2ecb73c82ed6bd316
JeonSant <78799671+JeonSant@users.noreply.github.com>
2022-06-17 20:41:52 +0700

    Add routes
    - routing untuk menampilkan list sapi (all)
    - routing untuk menambahkan instance sapi
    - routing untuk mencari sapi berdasarkan nama
    - routing untuk mencari sapi berdasarkan id
    - routing untuk mengubah data sapi berdasarkan id
    - routing untuk meghapus sapi berdasarkan id

--- website-sapi (bagian1)/routes/api.php
+++ website-sapi (bagian1)/routes/api.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Http\Controllers\SapiController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +14,24 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Public Routes
+Route::get('/sapis', [SapiController::class, 'index']); // routing untuk menampilkan list sapi (all)
+
+Route::post('/sapis', [SapiController::class, 'store']); // routing untuk menambahkan instance sapi
+
+Route::get('/sapis/search/{name}', [SapiController::class, 'search']); // routing untuk mencari sapi berdasarkan nama
+
+Route::get('/sapis/{id}', [SapiController::class, 'show']); // routing untuk mencari sapi berdasarkan id
+
+Route::put('/sapis/{id}', [SapiController::class, 'update']); // routing untuk mengubah data sapi berdasarkan id
+
+Route::delete('/sapis/{id}', [SapiController::class, 'destroy']); // routing untuk meghapus sapi berdasarkan id
+
+
+
+
+
+// Protected Routes
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
